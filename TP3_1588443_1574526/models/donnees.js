4cdd72500eb100972075e8a91aa079eb570d4e0d
angosa <antoine.gosselin@polymtl.ca>
2015-10-29 03:44:56 -0400

    Lab fini + remise faite

---
+++ TP3_1588443_1574526/models/donnees.js
@@ -0,0 +1,71 @@
+disciplines = {
+    CAMOUFLAGE : "Camouflage",
+    CHASSE : "Chasse",
+    SIXIEME_SENS: "Sixième Sens",
+    ORIENTATION: "Orientation",
+    GUERISON: "Guérison",
+    MAITRISE_DES_ARMES: "Maîtrise des Armes",
+    BOUCLIER_PSYCHIQUE: "Bouclier Psychique",
+    PUISSANCE_PSYCHIQUE: "Puissance Psychique",
+    COMMUNICATION_ANIMALE: "Communication Animale",
+    MAITRISE_PSYCHIQUE_DE_LA_MATIERE: "Maîtrise Psychique de la Matière"
+};
+
+equipement = {
+    EPEE: "Épée",
+    SABRE: "Sabre",
+    LANCE: "Lance",
+    MASSE_D_ARMES: "Masse d'Armes",
+    MARTEAU_DE_GUERRE: "Marteau de guerre",
+    HACHE: "Hache",
+    BATON: "Bâton",
+    GLAIVE: "Glaive",
+    GILET_DE_CUIR_MATELASSE: "Gilet de Cuir Matelassé",
+    POTION_DE_LAMPSUR: "Potion de Lampsur",
+    RATIONS_SPECIALES: "Rations Spéciales"
+};
+
+maitriseDArme = {
+    POIGNARD : "Le poignard",
+    LANCE : "La lance",
+    MASSE_D_ARMES : "La masse d’armes",
+    SABRE : "Le sabre",
+    MARTEAU_DE_GUERRE : "Le marteau de guerre",
+    EPEE : "L’épée",
+    HACHE : "La hâche",
+    BATON : "Le bâton",
+    GLAIVE : "Le glaive",
+    AUCUN : "Aucune maitrise d'arme"
+}
+
+objetSpecial = {
+    HUILE_DE_BAKNAR : "Huile de Backnar"
+}
+
+var infoPages = {
+	"p1":   [1, [160, 273]],   
+	"p12":  [3, [180, 259]], 
+	"p57":  [2, [331]],   
+	"p62":  [2, [288]],   
+	"p70":  [1, [209, 339]],
+	"p78":  [2, [245]],
+	"p85":  [1, [300]],
+	"p91":  [1, [134]],  
+	"p129": [2, [155]], 
+	"p134": [1, [57, 188, 331]],  
+	"p155": [2, [248, 191]], 
+	"p160": [1, [204, 318, 78]],
+	"p167": [1, [85, 300]],  
+	"p172": [1, [134]], 
+	"p180": [2, [70, 129]],
+	"p188": [1, [331]],    
+	"p204": [1, [134]], 
+	"p209": [2, [155]], 
+	"p245": [1, [91, 172]],   
+	"p248": [1, []],   
+	"p288": [1, [167]],   
+	"p300": [1, [12, 238]],
+	"p318": [1, [134]],  
+	"p331": [2, [62, 288]],
+	"p339": [1, []], 
+}
