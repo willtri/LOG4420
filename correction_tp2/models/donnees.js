979273a50de80faf9a25b89951eaf085aafc2bfb
willtri <tri-khiem-william.truong@polymtl.ca>
2015-10-28 23:27:31 -0400

    ajout des calculs de points d'habileter et d'endurence

--- correction_tp2/models/donnees.js
+++ correction_tp2/models/donnees.js
@@ -25,6 +25,19 @@ equipement = {
     RATIONS_SPECIALES: "Rations Spéciales"
 };
 
+maitriseDArme = {
+    POIGNARD : "Le poignard",
+    LANCE : "La lance",
+    MASSE_D_ARMES : "La masse d’armes",
+    SABRE : "Le sabre",
+    MARTEAU_DE_GUERRE : "Le marteau de guerre",
+    EPEE : "L’épée",
+    HACHE : "La hâche",
+    BATON : "Le bâton",
+    GLAIVE : "Le glaive",
+    AUCUN : "Aucune maitrise d'arme"
+}
+
 objetSpecial = {
     HUILE_DE_BAKNAR : "Huile de Backnar"
 }
--- correction_tp2/routes/index.js
+++ correction_tp2/routes/index.js
@@ -62,16 +62,104 @@ router.post('/creationJoueur', function (req, res) {
             messageErreurDiscipline: messageErreurDiscipline,
             messageErreurEquipement: messageErreurEquipement
         });
+
     }
 
     req.session.joueur = req.body;
     req.session.joueur.objetSpecial = undefined;
     req.session.joueur.enduranceTotal = 20 + Math.floor((Math.random() * 9) + 1);
     req.session.joueur.enduranceRestant = req.session.joueur.enduranceTotal;
+    req.session.joueur.enduranceDeBase = req.session.joueur.enduranceTotal;
     req.session.joueur.habileteDeBase = 10 + Math.floor((Math.random() * 9) + 1);
-    req.session.joueur.habilete = req.session.joueur.habileteDeBase
+    req.session.joueur.habilete = req.session.joueur.habileteDeBase;
     req.session.joueur.pieceDOr = 10 + Math.floor((Math.random() * 9) + 1);
-    console.log(req.session.joueur);
+    req.session.joueur.maitriseDArme = maitriseDArme.AUCUN;
+    // chiffre aléatoire pour choisir une maîtrise d’une arme
+    for (i = 0; i < req.session.joueur.disciplines.length; i++) {
+        if (req.session.joueur.disciplines[i] == disciplines.MAITRISE_DES_ARMES) {
+            var calcul_maitrise = Math.floor((Math.random() * 9) );
+            console.log(calcul_maitrise);
+            switch (calcul_maitrise) {
+                case 0:
+                    req.session.joueur.maitriseDArme = maitriseDArme.POIGNARD;
+                    break;
+                case 1:
+                    req.session.joueur.maitriseDArme = maitriseDArme.LANCE;
+                    break;
+                case 2:
+                    req.session.joueur.maitriseDArme = maitriseDArme.MASSE_D_ARMES;
+                    break;
+                case 3:
+                    req.session.joueur.maitriseDArme = maitriseDArme.SABRE;
+                    break;
+                case 4:
+                    req.session.joueur.maitriseDArme = maitriseDArme.MARTEAU_DE_GUERRE;
+                    break;
+                case 5:
+                    req.session.joueur.maitriseDArme = maitriseDArme.EPEE;
+                    break;
+                case 6:
+                    req.session.joueur.maitriseDArme = maitriseDArme.HACHE;
+                    break;
+                case 7:
+                    req.session.joueur.maitriseDArme = maitriseDArme.BATON;
+                    break;
+                case 8:
+                    req.session.joueur.maitriseDArme = maitriseDArme.GLAIVE;
+                    break;
+            }
+        }
+    }
+    // calcul des bonus
+    for (i = 0; i < req.body.equipement.length; i++) {
+        switch (req.body.equipement[i]) {
+            case equipement.BATON:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.BATON){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.EPEE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.EPEE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.GILET_DE_CUIR_MATELASSE:
+                req.session.joueur.enduranceTotal = req.session.joueur.enduranceTotal + 2;
+                req.session.joueur.enduranceRestant = req.session.joueur.enduranceTotal;
+                break;
+            case equipement.GLAIVE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.GLAIVE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.HACHE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.HACHE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.LANCE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.LANCE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.MARTEAU_DE_GUERRE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.MARTEAU_DE_GUERRE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.MASSE_D_ARMES:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.MASSE_D_ARMES){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+            case equipement.SABRE:
+                if (req.session.joueur.maitriseDArme == maitriseDArme.SABRE){
+                    req.session.joueur.habilete = req.session.joueur.habilete + 2;
+                }
+                break;
+        }
+    }
+    //console.log(req.session.joueur);
     res.redirect('/jeu/1');
 });
 
